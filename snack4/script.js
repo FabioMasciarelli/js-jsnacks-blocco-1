62e6b70c1af68069b4fcd1e950190bd27efd3270
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-04-02 14:18:34 +0200

    add: snack4

---
+++ snack4/script.js
@@ -0,0 +1,31 @@
+// In un array sono contenuti i nomi degli invitati alla festa del grande
+// Gatsby, chiedi all'utente il suo nome e comunicagli se può partecipare o
+// no alla festa.
+
+const array = ["pippo", "pluto", "topolino"]
+
+// chiediamo all'user il suo nome
+
+const username = prompt("qual'è il tuo nome?");
+
+// dichiariamo una variabile di appoggio
+let flag = false;
+
+for (i = 0; i < array.lenght; i++ ) {
+
+    if (username === array[i]) {
+        flag = true;
+    } 
+}
+
+let outputMessage ="";
+
+if (flag == true) {
+    outputMessage = `sei invitato!`;
+} else {
+    outputMessage = `non sei stato invitato`;
+}
+
+const result = document.querySelector(".result");
+
+result.innerHTML = outputMessage;
