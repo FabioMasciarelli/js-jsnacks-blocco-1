87a3c079c7cb3a8b5616531aa3b5acbc98525767
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-04-02 13:59:19 +0200

    add: scaffolding and snack3

---
+++ snack3/script.js
@@ -0,0 +1,24 @@
+// Il software deve chiedere per 10 volte all'utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+// chiediamo 10 numeri all'utente
+
+const num1 = parseInt(prompt ("inserisci numero"));
+const num2 = parseInt(prompt ("inserisci numero"));
+const num3 = parseInt(prompt ("inserisci numero"));
+const num4 = parseInt(prompt ("inserisci numero"));
+const num5 = parseInt(prompt ("inserisci numero"));
+const num6 = parseInt(prompt ("inserisci numero"));
+const num7 = parseInt(prompt ("inserisci numero"));
+const num8 = parseInt(prompt ("inserisci numero"));
+const num9 = parseInt(prompt ("inserisci numero"));
+const num10 = parseInt(prompt ("inserisci numero"));
+
+
+// sommiamo tutti i numeri 
+
+const total = num1 + num2 +  num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+
+// stampiamo la somma di tutti i numeri
+
+console.log(total);
